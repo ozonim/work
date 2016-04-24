b6a85576fb8a8680420e4f2af87d3d5a41975cfd
Oleg Nazarenko <ba.loo@yandex.ru>
2016-04-24 09:58:42 +0300

    240416

---
+++ Mocha_test/test.js
@@ -0,0 +1,9 @@
+describe("pow", function() {
+
+  it("возводит в n-ю степень", function() {
+    assert.equal(pow(2, 3), 8);
+  });
+
+});
+
+http://plnkr.co/edit/nRj3LNnHn8CcQxr4iJoo?p=preview    // Образец
